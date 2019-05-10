6c3beada977ed7b403586f4297451813e6e718b4
HongliangWang <whl7770@gmail.com>
2019-05-10 16:31:45 +0800

    add bootstrap4 & serialport functions

--- src/app/components/serialport/serialport.component.ts
+++ src/app/components/serialport/serialport.component.ts
@@ -8,16 +8,21 @@ import { SerialHardware } from "../../entity/serialhardware";
   styleUrls: ['./serialport.component.scss']
 })
 export class SerialportComponent implements OnInit {
+  public selectedPortId: string;
+  public portstate: boolean ;
+  public inputData: string = '';
 
   avalibaleHardwares:SerialHardware[];
-
-  constructor(private serialportService: SerialportService) { }
+  constructor(private serialportService: SerialportService) { 
+    this.portstate=false;
+  }
 
   ngOnInit() {
 
   }
 
   scanHardwares(): void {
+    this.selectedPortId = "";
     this.serialportService.scanHardwares()
         .subscribe(avalibaleHardwares => this.avalibaleHardwares = avalibaleHardwares);   //注意订阅不会等待，异步执行！！所以下面语句无法正确打印,加一个timeout就可以
     // setTimeout(()=>{
@@ -26,5 +31,31 @@ export class SerialportComponent implements OnInit {
     //   });
     // },100);
   }
+
+  selectPort($event) {
+    console.log($event.target.textContent);
+    this.selectedPortId = $event.target.textContent;
+    this.avalibaleHardwares = this.avalibaleHardwares.filter(
+      element => element.comName === this.selectedPortId
+    );
+  }
+
+  openSelectPort(selectedPortId:string){
+    this.serialportService.openSelectPort(selectedPortId)
+      .subscribe(portstate => this.portstate = portstate); 
+
+  }
+
+  closePort(selectedPortId:string){ 
+    this.serialportService.closePort(selectedPortId)
+      .subscribe(portstate => {
+        this.portstate = portstate;
+        this.selectedPortId = "";
+      }); 
+  }
+
+  sendData(inputData:string){
+    this.serialportService.sendData(inputData);
+    }
 
 }
--- src/app/providers/serialport.service.ts
+++ src/app/providers/serialport.service.ts
@@ -13,14 +13,14 @@ export class SerialportService {
 
   public serialhardwareList: SerialHardware[];
   public port: any;
-  public selectedPortId: string;
   public portOpts = { baudRate: 115200, autoOpen: false };
+  public receiveData:string;
+  public timeout: any;
 
   ngOnInit() {
   }
 
   scanHardwares(): Observable<SerialHardware[]> {
-    this.selectedPortId = ''
     let index = 1
     let portDetails: any
     this.serialhardwareList = []; // clear before every scan
@@ -41,7 +41,60 @@ export class SerialportService {
     return of(this.serialhardwareList);
   }
 
+  openSelectPort(selectedPortId: string): Observable<boolean> {
+    this.port = new this.electronService.serialPort(
+      selectedPortId,
+      this.portOpts,
+      err => {
+        if (err) {
+          console.log('Error opening port: ', err.message);
+          return of(false);
+        }
+      }
+    );
 
+    this.port.on('open', () => {
+      console.log('[LOG] Port opened: ' + selectedPortId);
+    });
 
+    this.port.on('data', data => {
+      this.receiveData = data;
+      console.log('[ReceiveDATA]: ' + data);
+    });
+
+    this.port.open(err => {
+      if (err) {
+        console.log('[ERR] Error opening port: ' + selectedPortId);
+        return of(false);
+      }
+    });
+    return of(true);
+  }
+
+  closePort(selectedPortId: string): Observable<boolean> {
+    this.port.close(err => {
+      if (err) {
+        console.log(err.message);
+      }
+    });
+    this.port = null;
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+    this.scanHardwares();
+    console.log('[LOG] Port closed: ' + selectedPortId);
+    return of(false);
+  }
+
+  sendData(inputData) {
+    inputData = inputData.toString(); // force conversion
+    let buf = new Buffer(inputData + '\r', 'utf8'); // append CR
+
+    this.port.write(buf, err => {
+      if (err) {
+        return console.log(`[LOG] Error on <${inputData}> command: `, err.message);
+      }
+    });
+  }
 
 }
